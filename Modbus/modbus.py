b4989b00f7e60787985fab10ac78cb73f8826f75
nonas-hunter <lukenonashunter@gmail.com>
2020-07-27 12:42:52 -0400

    Successful Client-Server Connection

--- Modbus/modbus.py
+++ Modbus/modbus.py
@@ -1,13 +1,15 @@
 """ Testing Modbus Protocol using PyModbus
-    Investing running Modbus as its own process on the Pi """
+    Investing running Modbus as its own process on the Pi
+    https://pymodbus.readthedocs.io/en/latest/index.html """
 
 # Import Necessary Modules for Asycronous Communication
-from twisted.internet import serialport, reactor
-from twisted.internet.protocol import ClientFactory
+# from twisted.internet import serialport, reactor
+# from twisted.internet.protocol import ClientFactory
+# from pymodbus.factory import ClientDecoder
+# from pymodbus.client.asynchronous.twisted import ModbusClientProtocol
 
 # Import Necessary Modules for Syncronous Communication
-from pymodbus.factory import ClientDecoder
-from pymodbus.client.asynchronous.twisted import ModbusClientProtocol
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 
 # Import Propper Framer (RTU -- Page 36 of Manuel)
 from pymodbus.transaction import ModbusRtuFramer as ModbusFramer
@@ -22,9 +24,21 @@ log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
 # Define Device Address
-UNIT = 0x1
+UNIT = 0x0
 
 def run_sync_client():
     """ Main loop for Modbus communication """
-    client = ModbusClient(method='rtu', port='/dev/ptyp0', timeout=1, baudrate=9600)
+    # Define client connection
+    client = ModbusClient(method='rtu', port='/dev/tty0', timeout=1, baudrate=9600)
     client.connect()
+
+    # Specify target device
+    log.debug("Reading Coils")
+    rr = client.read_coils(1, 1, unit=UNIT)
+    log.debug(rr)
+
+    # Close Client
+    client.close()
+
+if __name__ == "__main__":
+    run_sync_client()
